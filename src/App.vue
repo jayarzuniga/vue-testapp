9d98abc9ad423e2fe881a45aac90e4f0c1af0051
Jay-r <jhayar.zuniga004@gmail.com>
2024-09-17 17:25:04 +0800

    Created half the Home Page of the Application

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
+<script setup>
+  import Navbar from '@/components/Navbar.vue';
+  import Hero from '@/components/Hero.vue';
+  import HomeCards from '@/components/HomeCards.vue';
+  import JobListings from '@/components/JobListings.vue'
+</script>
+
 <template>
-  <h1 class="text-3xl text-bold">Vue Jobs</h1>
+  <Navbar />
+  <Hero title="Test Title" subtitle="Test Subtitle"/>
+  <HomeCards />
+  <JobListings />
 </template>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<template>
+    Job Listings
+</template>
